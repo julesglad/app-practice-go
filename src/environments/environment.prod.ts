ad359c7a3e5e8eea3748b03cfd6bad1500423e16
Julie <gladden.julie@gmail.com>
2023-04-11 20:41:58 -0500

    Prod environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdSGnlQs7uqSu68v9S5KmQFSayuErlRbU",
+    authDomain: "practice-go-21b09.firebaseapp.com",
+    databaseURL: "https://practice-go-21b09-default-rtdb.firebaseio.com",
+    projectId: "practice-go-21b09",
+    storageBucket: "practice-go-21b09.appspot.com",
+    messagingSenderId: "735147212615",
+    appId: "1:735147212615:web:28e8ad8e58d4cf8e855662",
+    measurementId: "G-2JVHQNNW40"
+  }
 };
